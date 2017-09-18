8b660569c1d1ea722de5d5994f38681d09b2ac19
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-18 16:09:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,15 +10,39 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100) + 1;
+
+  var cart_item = {}
+
+  cart_item[item] = price;
+
+  cart.push(cart_item);
+
+  console.log(item + " has been added to your cart.");
+
+  return cart
+
 }
 
 function viewCart() {
-  // write your code here
+
+  if (cart.length === 0) {
+    console.log("Your shopping cart is empty.");
+  } else {
+    var items = [];
+
+    for (var i = 0; i <= cart.length; i++) {
+      for (item in cart[i]) {
+        cart.push(item + "at $" + cart[i][item]);
+      }
+    }
+  }
 }
 
 function total() {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    return cart[i][item]
+  }
 }
 
 function removeFromCart(item) {
