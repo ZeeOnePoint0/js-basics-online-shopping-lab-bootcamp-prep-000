ea28957fb30a2023fe7b03c9c17d64d8649a890f
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-18 17:15:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,7 +75,8 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if (!cardNumber) {
     console.log("Sorry, we do not have a credit card on file for you.")
-  }
+  } else {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+  }
   setCart(new Array);
 }
