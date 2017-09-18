de43940b180d447797e5c2f2dc8f43649dc9f8ac
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-18 16:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addToCart(item) {
 
   cart.push(items);
 
-  console.log(item + " has been added to your cart");
+  console.log(item + " has been added to your cart.");
 
   return cart;
 
