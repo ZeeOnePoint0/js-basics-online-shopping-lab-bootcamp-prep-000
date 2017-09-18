cb56ba3c51c6a09f23cdaf2299657fd2699915cb
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-18 17:14:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,5 +77,5 @@ function placeOrder(cardNumber) {
     console.log("Sorry, we do not have a credit card on file for you.")
   }
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
-  cart = [];
+  setCart(new Array);
 }
