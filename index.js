e5b2616f20da93f46a01c438ef231169a96004d4
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-18 17:17:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,18 +28,29 @@ function addToCart(item) {
 }
 
 function viewCart() {
+  
   if (cart.length === 0) {
+    
     console.log("Your shopping cart is empty.")
+    
   } else {
+    
       var items = [];
 
       for (let i = 0; i < cart.length; i++) {
+        
         for (let item in cart[i]) {
+          
           items.push(item + " at $" + cart[i][item])
+          
         }
+        
       }
+      
     console.log("In your cart, you have " + items.join(", ") + ".");
+    
   }
+  
 }
 
 function total() {
@@ -61,22 +72,39 @@ function removeFromCart(item) {
   var itemInCart = false
 
   for (let i = 0; i < cart.length; i++) {
+    
     if (cart[i].hasOwnProperty(item)) {
+      
       itemInCart = true;
+      
       cart.splice(i, 1);
+      
     }
+    
   }
+  
   if (!itemInCart) {
+    
     console.log("That item is not in your cart.")
+    
   }
+  
   return cart;
+  
 }
 
 function placeOrder(cardNumber) {
+  
   if (!!cardNumber ===  false) {
+    
     console.log("Sorry, we don't have a credit card on file for you.")
+    
   } else {
+    
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    
   }
+  
   setCart(new Array);
+  
 }
