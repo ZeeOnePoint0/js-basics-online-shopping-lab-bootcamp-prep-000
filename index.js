43f6dd865bc6d4c61181cbf9a6fc4c12297a7a9a
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-18 17:16:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (!!cardNumber ===  false) {
-    console.log("Sorry, we do not have a credit card on file for you.")
+    console.log("Sorry, we don't have a credit card on file for you.")
   } else {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
   }
